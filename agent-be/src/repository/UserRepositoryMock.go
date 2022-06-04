f971f3bf7a31dc646b8202a8ec58c0b5b36d0379
jelenac11 <52001925+jelenac11@users.noreply.github.com>
2022-06-05 00:11:19 +0200

    feat: implement registration

---
+++ agent-be/src/repository/UserRepositoryMock.go
@@ -0,0 +1,48 @@
+package repository
+
+import (
+	"agent-app/src/dto"
+	"agent-app/src/model"
+
+	"github.com/stretchr/testify/mock"
+)
+
+type UserRepositoryMock struct {
+	mock.Mock
+}
+
+func (u *UserRepositoryMock) AddUser(user *model.User) (int, error) {
+	args := u.Called(user)
+	if args.Get(1) == nil {
+		return args.Get(0).(int), nil
+	}
+	return -1, args.Get(1).(error)
+}
+
+func (u *UserRepositoryMock) DeleteUser(id int) error {
+	panic("implement me!")
+}
+
+func (u *UserRepositoryMock) GetByEmail(email string) (*dto.UserResponseDTO, error) {
+	args := u.Called(email)
+	if args.Get(1) == nil {
+		return args.Get(0).(*dto.UserResponseDTO), nil
+	}
+	return nil, args.Get(1).(error)
+}
+
+func (u *UserRepositoryMock) GetByID(id int) (*model.User, error) {
+	args := u.Called(id)
+	if args.Get(1) == nil {
+		return args.Get(0).(*model.User), nil
+	}
+	return nil, args.Get(1).(error)
+}
+
+func (u *UserRepositoryMock) Update(user *model.User) (*dto.UserResponseDTO, error) {
+	args := u.Called(user)
+	if args.Get(1) == nil {
+		return args.Get(0).(*dto.UserResponseDTO), nil
+	}
+	return nil, args.Get(1).(error)
+}
